0857f4a24f2cd662b8be7d95b5f33d32650fe218
kmensink95 <kmensink95@gmail.com>
2020-08-21 22:22:36 +0200

    styling first page mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'rock-paper-scisso';
+export class AppComponent implements OnInit {
+  choice = '';
+
+  constructor(){}
+
+  ngOnInit(): void {
+
+  }
 }
